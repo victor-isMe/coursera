838236c5a01e8ef35a2195ff61adee5f6dabe470
victor-isMe <muhammadfaizniwansyah23@gmail.com>
2025-08-04 10:38:48 -0400

    change js cript file

--- SampleFolder/Scope/scope_lab.js
+++ SampleFolder/Scope/scope_lab.js
@@ -1,21 +1,13 @@
-//Global scope
-var globalVar = "Saya adalah variabel global";
-let globalLet = "Saya juga global, tetapi dibatasi dengan let";
-const globalConst = "Saya adalah konstanta global";
-
+//tugas praktik
 {
-//Block scope
-var blockVar = "Saya adalah var yang dibatasi blok";
-let blockLet = "Saya adalah let yang dibatasi oleh blok";
-const blockConst = "Saya adalah const yang dibatasi oleh blok";
-}
+let blockLet = "Ini adalah let";
+const blockConst = "Ini adalah const";
+var blockVar = "Ini adalah var";
 
-//Global scope output
-console.log(globalVar);
-console.log(globalLet);
-console.log(globalConst);
-
-//Block scope output
-console.log(blockVar);
 console.log(blockLet);
 console.log(blockConst);
+console.log(blockVar);
+}
+console.log(blockLet);
+console.log(blockConst);
+console.log(blockVar);
