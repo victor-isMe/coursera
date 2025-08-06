1714dd40ddfe8b9a77fbf8c439d516f8555f57ae
victor-isMe <muhammadfaizniwansyah23@gmail.com>
2025-08-06 01:38:14 -0400

    edit file with add user experince field

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -6,6 +6,7 @@ function submitFeedback(){
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const experince = document.getElementById('experince').value;
 
     alert('Thank you for your valuable feedback');
 
@@ -16,6 +17,7 @@ function submitFeedback(){
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperince').innerHTML = experince;
 
     document.getElementById('userInfo').style.display = 'block';
 
